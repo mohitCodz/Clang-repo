5b8a916c5d9f9d6f59d34564811642bb7fba3606
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-28 23:07:41 +0530

    Create traversalSearch.c

---
+++ functionn/traversalSearch.c
@@ -0,0 +1,19 @@
+// program showing travesal search
+
+#include "stdio.h"
+
+ int main()
+    {
+
+         int arr[5] = {1, 2, 3, 4, 5}; // aray of element 5
+          int target = 4;
+        for (int i = 0; i < 5; i++) // binary search pattren
+        {
+             if (arr[i] == target) // slective statement
+             {
+                printf("Element found at index %d\n", i);
+                break;  // exit the loop
+             }
+        }
+        
+    }
