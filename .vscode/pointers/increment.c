470e353738693576e03f18c263e27dea53a94958
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-28 12:32:48 +0530

    Update increment.c

--- .vscode/pointers/increment.c
+++ .vscode/pointers/increment.c
@@ -1,19 +1,14 @@
-// increment 
+// increment
 
 #include "stdio.h"
 int main()
 {
-    int a = 2;
-    int b = 4;
-    int *ptr1 = &a;
-    int *ptr2 = &b; 
-    if (*ptr1 < *ptr2)
-    {
-        printf("%d is less then %d",*ptr1 ,*ptr2);
-    } else if (*ptr2 > *ptr1)
-    {
-        printf("%d is greater then %d",*ptr2 ,*ptr1);
-    }
-    
+    int a[10] = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    int *ptr1;
+    ptr1 = &a[2];
+    printf(" Before %d\n", *ptr1);
+    ptr1++;
+    printf(" After %d\n", *ptr1);
+    getchar();
     return 0;
 }
