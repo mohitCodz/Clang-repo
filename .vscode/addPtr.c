1440dd56727135604658f94da2f0d33f931a93aa
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-28 12:32:50 +0530

    Update addPtr.c

--- .vscode/addPtr.c
+++ .vscode/addPtr.c
@@ -3,14 +3,8 @@
 int main() {
   int a = 101;
   int b = 201;
-  int c = 301;
-  int d = 401;
-  int e = 501;
   int *ptr1 = &a;
   int *ptr2 = &b;
-  int *ptr3 = &c;
-  int *ptr4 = &d;
-  int *ptr5 = &e;
-  printf("%d + %d + %d + %d + %d = %d\n", *ptr1, *ptr2, *ptr3, *ptr4, *ptr5, *ptr1 + *ptr2 + *ptr3 + *ptr4 + *ptr5);
+  printf("%d + %d = %d\n", *ptr1, *ptr2, *ptr1 + *ptr2);
   return 0;
 }
